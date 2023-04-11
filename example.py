5f98b11772e4e6e1b3ee1c024e00e0a82c4a2012
yearlongmason <114454215+yearlongmason@users.noreply.github.com>
2023-04-11 15:31:41 -0400

    Update example.py

--- example.py
+++ example.py
@@ -9,7 +9,7 @@ High = 100000
 
 # Compute x^2 + y^2 across a 2D grid
 x, y = np.meshgrid(range(Low, High, 5000), range(100, 0, -10))
-z = x ** y
+z = x * y
 
 # Convert this grid to columnar data expected by Altair
 model = pd.DataFrame({'x': x.ravel(),
